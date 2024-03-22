d8c4c1a77dec581031b7633096aba12baff7911c
Code021-raelle <gabrielakinshola3@gmail.com>
2024-03-22 08:55:42 -0700

    bash

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,7 +5,7 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
   path    => ['/usr/bin', '/usr/local/bin'],
   unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
   require => Package['python3-pip'],
