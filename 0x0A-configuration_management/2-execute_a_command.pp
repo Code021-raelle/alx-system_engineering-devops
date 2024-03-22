19212403bc416d92b08f051217adc1c866fdbca8
Code021-raelle <gabrielakinshola3@gmail.com>
2024-03-22 08:28:29 -0700

    bash

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # 2-execute_a_command.pp
 
-exec { 'killmenow':
-  command => '/usr/bin/pkill -f killmenow',
-  path    => ['/usr/bin', '/usr/sbin', '/bin']
-  onlyif  => 'pgrep -f killmenow',
+exec { 'pkill':
+  command  => '/usr/bin/pkill -f killmenow',
+  path     => ['/usr/bin', '/usr/sbin', '/bin']
 }
