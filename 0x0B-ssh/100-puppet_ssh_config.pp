a9b955ba8389800a1e9f509282d2df74fb412456
Code021-raelle <gabrielakinshola3@gmail.com>
2024-03-23 07:20:55 -0700

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # 100-puppet_ssh_config.pp
 
-file_line { 'Turn off password auth':
+File_line { 'Turn off password auth':
   path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   ensure => present,
 }
 
-file_line { 'Declare identity file':
+File_line { 'Declare identity file':
   path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
   ensure => present,
