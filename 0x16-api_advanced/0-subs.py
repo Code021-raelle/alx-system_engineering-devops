10172aa92e7b3679289a646cf901ac0bde514c95
Code021-raelle <gabrielakinshola3@gmail.com>
2024-05-07 15:04:39 +0100

    reddit_api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -30,12 +30,3 @@ def number_of_subscribers(subreddit):
             return 0
     else:
         return 0
-
-
-if __name__ == '__main__':
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
